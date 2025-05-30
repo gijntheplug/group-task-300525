05f38893b0395d791b14a0e09485097d38f8ef78
Andrea Fabbri <aandreafabbri@gmail.com>
2025-05-30 17:03:26 +0200

    Update Program.cs

--- Andrea Fabbri/Program.cs
+++ Andrea Fabbri/Program.cs
@@ -1,7 +1,29 @@
-public class Program
+public sealed class GestoreSpese
 {
-    static void Main(string[] args)
+    private static GestoreSpese _instance;
+    private static readonly object _lock = new object();
+    //TODO INSERIRE QUI LISTA PER LE COSE DA GESTIRE (SPESA)
+    private List<Spesa> spese = new List<Spesa>();
+    private GestoreSpese() { }
+    public static GestoreSpese Instance
     {
-        
+        get
+        {
+            if (_instance is null)
+            {
+                lock (_lock)
+                {
+                    if (_instance is null)
+                    {
+                        _instance = new GestoreSpese();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+    public void SalvaSpesa(SpeseFamiglia spese)
+    {
+        spese.Add(spesa)
     }
 }
